ca2e3c99a9898041c075a500e5e5c8c309bbed4b
sealfur <jkinal@thoughtworks.com>
2020-06-06 17:54:02 +1000

    finished chapter three. somehow still have an hour of lessons to go.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
+const yaml = require('js-yaml');
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./_site/images');
 	eleventyConfig.addPassthroughCopy('./_site/css');
 	eleventyConfig.addLayoutAlias('base', 'pageTemplates/base.njk');
 	eleventyConfig.addLayoutAlias('page', 'pageTemplates/page.njk');
 	eleventyConfig.addLayoutAlias('page-hero', 'pageTemplates/page-hero.njk');
+	eleventyConfig.addDataExtension('yaml', contents => yaml.safeLoad(contents))
 
 		return {
 			markdownTemplateEngine: "njk",
