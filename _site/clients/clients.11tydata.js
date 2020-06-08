e59b24a5038d0180bb66822210319aa9946ac8dc
sealfur <jkinal@thoughtworks.com>
2020-06-08 17:44:28 +1000

    got up to creating collections

---
+++ _site/clients/clients.11tydata.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+ module.exports = async function() {
+
+	return fetch('http://trainingxyz.com/api/users/all')
+	.then(res => res.json())
+	.then(json => {
+		return {
+			clients: json
+		}
+	})
+
+}
